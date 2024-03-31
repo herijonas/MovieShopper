6e969d381b8a97fccc1e4f9fe66904a18babae67
unknown <eduardoisraelstinghenpreuss@gmail.com>
2024-03-31 15:42:35 -0300

    MovieShopper project

---
+++ slider-data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Avengers: Endgame',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider1.jpg'
+    },
+    {
+        name: 'The Conjuring 3',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider2.jpg'
+    },
+    {
+        name: 'Indiana Jones',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider3.webp'
+    },
+    {
+        name: 'Killers of the Flower Moon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider5.jpg'
+    },
+    {
+        name: 'Duna',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider4.jpeg'
+    }
+]
